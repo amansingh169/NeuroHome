eda72150f3b3c4f99225c28690940e307779232f
oyedivyansh <divyansh.sharma1528@gmail.com>
2025-06-29 15:00:33 +0530

    grid intiiated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import ThemeSelector from "./components/ThemeSelector";
 import Home from "./pages/Home";
+import BentoGrid from "./components/BentoGrid";
 
 function App() {
   return (
     <div className="home-wrapper d-flex justify-content-center align-items-center p-4">
       <Home />
-      {/* <ThemeSelector /> */}
+      <ThemeSelector />
+    <div className="min-h-screen bg-black text-white">
+      <h1 className="text-4xl text-center pt-10 font-bold">Home Grid</h1>
+      <BentoGrid />
+    </div>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,9 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import "./SASS/main.scss";
 import App from "./App.jsx";
 import { ThemeProvider } from "./contexts/ThemeContext.jsx";
+import './tailwind.css';
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider>
