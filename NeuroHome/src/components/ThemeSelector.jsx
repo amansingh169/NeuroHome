bf2f10ead2d41a25920a2bf2b4d3859ec0694b34
Aman Singh <as2211075@gmail.com>
2025-07-01 14:25:19 +0530

    Added theme select

--- NeuroHome/src/components/ThemeSelector.jsx
+++ NeuroHome/src/components/ThemeSelector.jsx
@@ -1,24 +1,45 @@
 import { useContext } from "react";
 import { ThemeContext } from "../contexts/ThemeContext";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 
-const ThemeSelector = ({ num }) => {
+const ThemeSelector = () => {
   const { theme, setTheme, availableThemes } = useContext(ThemeContext);
 
   return (
-    <select
-      className="form-select"
-      value={theme}
-      onChange={(e) => {
-        setTheme(e.target.value);
-      }}
-      style={{ width: "300px" }}
-    >
-      {availableThemes.map((theme) => (
-        <option value={theme} key={theme}>
-          {theme.charAt(0).toUpperCase() + theme.slice(1)} Theme
-        </option>
-      ))}
-    </select>
+    // <select
+    //   className="form-select"
+    //   value={theme}
+    //   onChange={(e) => {
+    //     setTheme(e.target.value);
+    //   }}
+    //   style={{ width: "300px" }}
+    // >
+    // {availableThemes.map((theme) => (
+    //   <option value={theme.name} key={theme.name}>
+    //     {theme.icon}
+    //     {theme.name.charAt(0).toUpperCase() + theme.name.slice(1) + " Theme"}
+    //   </option>
+    // ))}
+    // </select>
+
+    <Select>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue />
+      </SelectTrigger>
+      <SelectContent>
+        {availableThemes.map((theme) => (
+          <SelectItem value={theme.name}>
+            {theme.name.charAt(0).toUpperCase() + theme.name.slice(1) + " Theme"}
+          </SelectItem>
+        ))}
+      </SelectContent>
+    </Select>
   );
 };
 
--- NeuroHome/src/contexts/ThemeContext.jsx
+++ NeuroHome/src/contexts/ThemeContext.jsx
@@ -1,9 +1,17 @@
 import { createContext, useState, useEffect } from "react";
+import { Sun, SunDim, SunMoon, Moon, Zap, Flame } from "lucide-react";
 
 export const ThemeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const availableThemes = ["light", "ash", "dark", "onyx", "neon", "fire"];
+  const availableThemes = [
+    { name: "light", icon: <Sun className="w-4 h-4" /> },
+    { name: "ash", icon: <SunDim className="w-4 h-4" /> },
+    { name: "dark", icon: <SunMoon className="w-4 h-4" /> },
+    { name: "onyx", icon: <Moon className="w-4 h-4" /> },
+    { name: "neon", icon: <Zap className="w-4 h-4" /> },
+    { name: "fire", icon: <Flame className="w-4 h-4" /> },
+  ];
 
   const [theme, setTheme] = useState("dark");
 
